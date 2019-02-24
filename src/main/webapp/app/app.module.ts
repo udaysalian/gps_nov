22f5337503e4fb84315f0db12a5a4e32732ae030
vagrant <vagrant@vagrant.vm>
2019-02-24 04:57:06 +0000

    remote unwanted components

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -1,6 +1,5 @@
 import './vendor.ts';
 
-
 import { NgModule, Injector } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -20,11 +19,10 @@ import { NetraAccountModule } from './account/account.module';
 import { NetraEntityModule } from './entities/entity.module';
 import * as moment from 'moment';
 import { NetraAppNominationNewModule } from './nomination/nominationnew.module';
-import { NetraAppPlacesModule } from './places/places.module';
-import { NetraAppPathNominationModule } from './path-nomination/path-nomination.module';
+
 // jhipster-needle-angular-add-module-import JHipster will add new module here
 import { JhiMainComponent, NavbarComponent, FooterComponent, PageRibbonComponent, ErrorComponent } from './layouts';
-import {AgGridModule} from 'ag-grid-angular';
+import { AgGridModule } from 'ag-grid-angular';
 @NgModule({
     imports: [
         BrowserModule,
@@ -35,9 +33,8 @@ import {AgGridModule} from 'ag-grid-angular';
         NetraHomeModule,
         NetraAccountModule,
         NetraEntityModule,
-        NetraAppNominationNewModule,
-        NetraAppPlacesModule,
-        NetraAppPathNominationModule,
+        NetraAppNominationNewModule
+
         // jhipster-needle-angular-add-module JHipster will add new module here
     ],
     declarations: [JhiMainComponent, NavbarComponent, ErrorComponent, PageRibbonComponent, FooterComponent],
--- src/main/webapp/app/path-nomination/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './path-nomination.component';
-export * from './path-nomination.route';
-export * from './path-nomination.module';
