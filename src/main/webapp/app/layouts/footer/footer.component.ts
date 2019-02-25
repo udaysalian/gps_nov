ec32058d598e3bd5bdf60849a036c70d7755ea92
vagrant <vagrant@vagrant.vm>
2019-02-25 05:14:56 +0000

    update home page and footer

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { currentId } from 'async_hooks';
 
 @Component({
     selector: 'jhi-footer',
     templateUrl: './footer.component.html'
 })
-export class FooterComponent {}
+export class FooterComponent {
+    currentYear: number;
+    constructor() {
+        this.currentYear = new Date().getFullYear();
+    }
+}
